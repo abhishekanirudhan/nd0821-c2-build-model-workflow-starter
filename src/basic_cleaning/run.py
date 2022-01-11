b681c500e8997f31164562305320fe771480a2e9
Abhishek Anirudhan <abhishek.anirudhan@gmail.com>
2022-01-11 16:34:21 +0530

    Enforcing geo bounds

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,10 @@ def go(args):
     logger.info("Converting last review from str to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Enforcing latitude and longitude bounds")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Writing out cleaned dataset")
     
     df.to_csv(args.output_artifact, index = False)
